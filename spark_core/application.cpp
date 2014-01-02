3a045a62c86550fef91fa32ea28017660faa0b27
Synox <synox@synox>
2014-01-02 16:32:55 +0100

    code format

--- spark_core/application.cpp
+++ spark_core/application.cpp
@@ -1,6 +1,7 @@
-
-/* Includes ------------------------------------------------------------------*/  
-#include "application.h"
+// configure the server and request
+char server[] = "example.com";
+char request[] = "GET /ampel/?from=Bern&to=Bern,Inselspital HTTP/1.0";
+#define DEBUG false
 
 /* Function prototypes -------------------------------------------------------*/
 void evalResponse(String& response);
@@ -9,56 +10,50 @@ void evalResponse(String& response);
 #define CHECK_INTERVAL_MILLIS 30000
 #define LED_MAX_VALUE 255 // dim the lights
 
-// configure the server and request
-char server[] = "example.ch";
-char request[] = "GET /ampel/?from=Bern&to=Bern,Inselspital HTTP/1.1";
-
-
-#define DEBUG false
 TCPClient client;
 
 /* This function is called once at start up ----------------------------------*/
 void setup() {
- Serial.begin(9600);
+    Serial.begin(9600);
 
-  RGB.control(true);
-  // dimmed blue
-  RGB.color(20, 10, 150);
+    RGB.control(true);
+    RGB.color(0,0,10);   // dimmed blue
 }
 
-
-char buffer [400];            // string to hold the text from server
-unsigned int nextTime = 0;
+char buffer [400];            // string to hold http response
+unsigned int nextTime = 0;    // next time to contact the server
 
 /* This function loops forever --------------------------------------------*/
 void loop() {
-	  if ( nextTime > millis() ) {
-	  	return;
-	  }
-	  
-      if(DEBUG) Serial.println("connecting to server...");
-      if (client.connect(server, 80))  {
+    if ( nextTime > millis() ) {
+        return;
+    }
+
+    if(DEBUG) Serial.println("connecting to server...");
+    if (client.connect(server, 80))  {
         if(DEBUG) Serial.println("connected");
         
         client.println(request);
-        client.println("HOST: synox.ch");
-        client.print("Connection: close\n");
+        client.print("HOST: ");
+        client.println(server);
+        client.println("Connection: close");
 
         client.println();
         client.flush();
-      } else  {
+    } else  {
         Serial.println("connection failed");
         client.stop();
         
+        // retry in 5 sec
         nextTime = millis() + 5*1000;
         return;
-      }
+    }
 
     //wait for request to be processed.
-    delay(1000);        
+    delay(1000);
 
     if(DEBUG) Serial.println("reading response....");
-    for(unsigned int i =0; i < sizeof(buffer) && client.available(); i++) {
+    for(unsigned int i=0; i < sizeof(buffer) && client.available(); i++) {
         char c = client.read();
         if(c == -1) {
             break;
@@ -68,20 +63,17 @@ void loop() {
     client.stop();
     
     String response(buffer);
+    evalResponse(response);
     
-		evalResponse(response);
-	
-    
-   nextTime = millis() + CHECK_INTERVAL_MILLIS;
+    nextTime = millis() + CHECK_INTERVAL_MILLIS;
 }
 
-
 void evalResponse(String& response) {
-		if(DEBUG)  Serial.println("parsing response");
-			
+    if(DEBUG)  Serial.println("parsing response");
+
     if ( response.indexOf("(--)off")!=-1) {
         Serial.println("OFF");
-        RGB.color(0, 0, 5);
+        RGB.color(0, 0, 0);
     } else if ( response.indexOf("(--)red") != -1) {
         Serial.println("RED");
         RGB.color(LED_MAX_VALUE, 0, 0);
@@ -93,6 +85,6 @@ void evalResponse(String& response) {
         RGB.color(0, LED_MAX_VALUE, 0);
     } else {
         Serial.println("unknown command: " + response);
-         RGB.color(0, 0, LED_MAX_VALUE);
+        RGB.color(0, 0, LED_MAX_VALUE);
     }
 }
