921f099f19f182d7d70e624c3a4211b4b4cebff3
Synox <synox@synox>
2014-01-02 16:22:47 +0100

    simplified code, use opendata interface

--- php_webservice/index.php
+++ php_webservice/index.php
@@ -1,65 +1,57 @@
 <?php 
 /* *
- * PHP Webservice for the Arduino Busstop Light
- *
- * Written by Bastian Widmer (bastianwidmer.ch)
- *
- * Just change the $url $walkingtime and $*Time to your needs.
- * 
+ * PHP Webservice for the Busstop Light
  * The Parameters from= and to= must be passed in the request. 
  * example: index.php?from=bern&to=Bern,Ostermundigen
- *
  * */
-include ('simple_html_dom.php');
 
-$walkingtime = 180; // Define walking Time to the Bus Stop
-$redTime = 60; // too late
-$orangeTime = 3*60; // run!
-$greenTime = 7*60; // walk now
-$offTime = $greenTime; // before green, it is off
-
-
-
-$url = 'http://fahrplan.search.ch/'.urldecode($_GET["from"]).'/'.urldecode($_GET["to"]).''; // Get the timetable data url from fahrplan.search.ch
-
-$html = file_get_html($url);
-
-// if there's a error returned in our query return error and the message and exit
-if ($html->find('div.sl_warning', 0)) {
-    echo "error - ".$html->find('div.sl_warning', 0)->plaintext;
-    exit;
+// check params
+if(!array_key_exists("from", $_GET) || !array_key_exists("to", $_GET) ) {
+	die("params from and to missing");
 }
 
 // set timezone
 date_default_timezone_set("Europe/Zurich");
 
 
-// TODO: it should show green if there is at least one good connection. (Problem now: if there are many connections, it shows always red)
+// http://transport.opendata.ch/#connections
+$url = 'http://transport.opendata.ch/v1/connections?from='.urldecode($_GET["from"]).'&to='.urldecode($_GET["to"]).'&fields[]=connections/from/departure&limit=3'; 
+$html = file_get_contents($url);
 
-// calculate next busstop
-foreach ($html->find('span.oev_printbold') as $e) {//Get the First DIV with StartData 
-    $time = substr(trim($e->innertext), 0, 5);
-    $nextbus = strtotime($time); // Strip unneccesary data and convert to a timestamp
-    $now = time(); 
-    $diff = $nextbus - $now; // calculate the difference for the events
-    echo "(--)";
-    if ($diff < $walkingtime) {
-        if ($diff < -60) {
-        	// if we get a negative value of time (can happen if the server time is not 100% accurate) skip this result and wait for the next bus
-          continue;
-        } 
-        echo "red - ".$diff;
+// example response:'{"connections":[{"from":{"departure":"2014-01-02T15:38:00+0100"}},{"from":{"departure":"2014-01-02T15:40:00+0100"}},{"from":{"departure":"2014-01-02T15:43:00+0100"}},{"from":{"departure":"2014-01-02T15:45:00+0100"}}]}';
+$connections = json_decode($html)->connections;
+
+// resulting colors, array of options. in the end the post color will be shown.
+$colors = array();
+$now = time(); 
+
+foreach($connections as $con) {
+	$timeStr =  $con->from->departure; 
+	$ts = strtotime($time); // Strip unneccesary data and convert to a timestamp
+
+  $diff = $nextbus - $now; // calculate the difference for the events
+    
+    if ($diff < 90) {
+    	// too late
+    	$colors["red"] = true;
+    } else if ($diff < 3 * 60 ) {
+    	// hurry
+    	$colors["orange"] = true;
+    } else if ($diff < 5 * 60 ) {
+    	// now it is good
+    	$colors["green"] = true;
     } else {
-        if ($diff <= $redTime) {
-            echo "red - ".$diff;
-        } if ($diff <= $orangeTime) {
-            echo "orange - ".$diff;
-        } if ($diff <= $greenTime) {
-            echo "green - ".$diff;
-        } else {
-            echo "off - ".$diff;
-        }
-    }
-    echo "\n";
-    exit;
+    	// longer, then turn led off
+    } 
 }
+if(array_key_exists("green",$colors) ) {
+	echo "(--)green\n";
+} else if(array_key_exists("orange",$colors) ) {
+	echo "(--)orange\n";
+} else if(array_key_exists("red",$colors) ) {
+	echo "(--)red\n";
+} else {
+	echo "(--)off\n";
+}
+exit;
+?>
