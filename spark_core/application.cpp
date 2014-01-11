8fb12f98561883735b60ebc489af5900bedfaa1b
Synox <synox@synox>
2014-01-11 04:37:47 +0100

    refactoring http request

--- spark_core/application.cpp
+++ spark_core/application.cpp
@@ -1,17 +1,23 @@
-// configure the server and request
-char server[] = "www.yourserver.com";
-char request[] = "GET /webservice.php?from=Bern&to=Bern,Inselspital HTTP/1.0";
-// HTTP Version 1.0 is used to avoid chunked transfer encoding
-
-#define DEBUG false
-
-/* Function prototypes -------------------------------------------------------*/
-void evalResponse(String& response);
-void animateGreen();
 
 // Configure the interval and brightness
 #define CHECK_INTERVAL_MILLIS 30000
 #define LED_MAX_VALUE 255 // dim the lights
+#define DEBUG true
+
+#include <string>
+
+
+const char* from = "Bern";
+const char* to = "Winterthur";
+const char* hostname = "server.ch";
+const char* basic_url = "/webservice.php?time=12:00&from=";
+
+
+/* Function prototypes -------------------------------------------------------*/
+void evalResponse(String& response);
+void animateColors();
+int http_request (const char* hostname, const char* path, char* response, unsigned int maxLen);
+
 
 TCPClient client;
 
@@ -60,42 +66,19 @@ void loop() {
         return;
     }
 
-    if(DEBUG) Serial.println("connecting to server...");
-    if (client.connect(server, 80))  {
-        if(DEBUG) Serial.println("connected");
-        
-        client.println(request);
-        client.print("HOST: ");
-        client.println(server);
-        client.println("Connection: close");
+    String path(basic_url);
+    path +=(from);
+    path +=("&to=");
+    path +=(to);
 
-        client.println();
-        client.flush();
-    } else  {
-        Serial.println("connection failed");
-        client.stop();
-        
-        // retry in 5 sec
-        nextTime = millis() + 5*1000;
-        return;
+    if( http_request(hostname, path, buffer, sizeof(buffer)) > 0 ) {
+        // ok
+        String response(buffer);
+        evalResponse(response);
+    } else {
+        // failed
     }
 
-    //wait for request to be processed.
-    delay(1000);
-
-    if(DEBUG) Serial.println("reading response....");
-    for(unsigned int i=0; i < sizeof(buffer) && client.available(); i++) {
-        char c = client.read();
-        if(c == -1) {
-            break;
-        }
-        buffer[i] = c;
-    }
-    client.stop();
-    
-    String response(buffer);
-    evalResponse(response);
-    
     nextTime = millis() + CHECK_INTERVAL_MILLIS;
 }
 
@@ -171,4 +154,36 @@ void animateColors() {
                 blue = 50;
             }
         }
+}
+
+
+
+int http_request (const char* hostname, String path, char* response, unsigned int maxLen) {
+
+    if(DEBUG) Serial.println("connecting to server...");
+    if (client.connect(hostname, 80))  {
+        if(DEBUG) Serial.println("connected");
+        client.print("GET ");        client.print(path);        client.print(" HTTP/1.0\n");
+        client.print("HOST: ");        client.println(hostname);        client.print("\n");
+        client.print("Connection: close\n\n");
+        client.flush();
+    } else  {
+        Serial.println("connection failed");
+        client.stop();
+        return 0;
+    }
+
+    // Block until first byte is read.
+    client.read();
+
+    if(DEBUG) Serial.println("reading response....");
+    for(unsigned int i=0; i < maxLen && client.available(); i++) {
+        char c = client.read();
+        if(c == -1) {
+            break;
+        }
+        response[i] = c;
+    }
+    client.stop();
+    return 1;
 }
